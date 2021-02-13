4c360324cc75efd44f4c0316b4c8f5ff12f7bcb0
Shwimble <nicolacortese92@gmail.com>
2021-02-13 17:51:55 +0000

    Grids and quads & git ignore

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(5f,20f)] public int gridSize = 10;
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+    }
+}
