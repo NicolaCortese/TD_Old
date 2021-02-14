058078749b6b5d2ce5c0d48635767a4509b301a1
Shwimble <nicolacortese92@gmail.com>
2021-02-14 18:07:45 +0000

    Labels

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
     [Range(5f,20f)] public int gridSize = 10;
-
+    TextMesh textMesh;
     void Update()
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+        
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+        transform.position = new Vector3(snapPos.x, 0, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
+        
+
     }
 }
