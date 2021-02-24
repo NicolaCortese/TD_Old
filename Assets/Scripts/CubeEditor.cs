71dcc581e1bf1f478ac013afa46702f3abef3d07
Shwimble <nicolacortese92@gmail.com>
2021-02-24 22:00:22 +0000

    Lecture 343 : Going Solo

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent(typeof(Waypoint))]
+
+public class CubeEditor : MonoBehaviour
+{
+
+   
+    
+    Waypoint waypoint;
+
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+        
+    }
+
+    void Update()
+    {
+        SnapToGrid();
+
+        LabelsGrid();
+
+    }
+
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+       
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x, 
+            0f,
+            waypoint.GetGridPos().y) * gridSize;
+    }
+
+    private void LabelsGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        string labelText = waypoint.GetGridPos().x + "," + waypoint.GetGridPos().y;
+        textMesh.text = labelText;
+
+        gameObject.name = labelText;
+    }
+}
---
+++ Assets/Tower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] Transform objectToPan;
+    [SerializeField] Transform targetEnemy;
+
+
+    private void OnParticleTrigger()
+    {
+        print("hit");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        objectToPan.LookAt(targetEnemy);
+    }
+}
